c5f6e4aceaf058a1102a2a259e8168e26b6fd6e5
Omer Ied <omeried73@gmail.com>
2020-10-05 00:23:50 +0300

    completed initial database

--- main-app/views.py
+++ main-app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from .models import Profile, Song, ProfileSong, Genre
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
