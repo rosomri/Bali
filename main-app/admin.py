2932992e10851bdae629a5ea01f70c45a29bed5f
Omer Ied <omeried73@gmail.com>
2020-10-06 11:06:05 +0300

    updated models

--- main-app/admin.py
+++ main-app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Song, ProfileSong, Genre
+from .models import Profile, Song, ProfileSong, Genre, ProfileGenre
 
 
 admin.site.register(Profile)
 admin.site.register(Song)
 admin.site.register(ProfileSong)
 admin.site.register(Genre)
+admin.site.register(ProfileGenre)
+
