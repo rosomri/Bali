e982cdd1d1f46645d6324b099ff6afcc54f2b770
Omri Rosner <68001413+rosomri@users.noreply.github.com>
2020-10-20 17:17:36 +0300

    register permission

--- account/api/views.py
+++ account/api/views.py
@@ -1,12 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 
 from account.api.serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
 
 
 @api_view(['POST'])
+@permission_classes(())
 def registration_view(request):
     serializer = RegistrationSerializer(data=request.data)
     data = {}
